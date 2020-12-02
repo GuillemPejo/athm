279fa5ee33922431b51ce4bac643777fe098b468
guillem <guillempejo@gmail.com>
2020-12-02 22:00:01 +0100

    Add FloatingButton funcionallity ready to implement

--- app/src/main/java/me/guillem/athm2app/Views/HomeActivity.java
+++ app/src/main/java/me/guillem/athm2app/Views/HomeActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +25,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -34,18 +41,33 @@ import me.guillem.athm2app.Utils.Utils;
 public class HomeActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, MenuItem.OnActionExpandListener {
 
     private RecyclerView rv;
+    private boolean clicked = false;
     public MaterialCardView card;
     public ProgressBar mProgressBar;
+    public FrameLayout layer_transparent;
     private FirebaseCRUD recurs_crud = new FirebaseCRUD();
     private LinearLayoutManager layoutManager;
-    RecyclerAdapterCardsHome adapter;
+    protected RecyclerAdapterCardsHome adapter;
+    private FloatingActionButton button_new, button_newvisit, button_newobra;
     private DatabaseReference mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+    private Animation fromBottom, rotateOpen, rotateClose, toBottom;
+    private TextView tnv, tno;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         card = findViewById(R.id.card);
+        layer_transparent = findViewById(R.id.shadow_layer);
+        button_new = findViewById(R.id.button_add_new);
+        button_newobra = findViewById(R.id.button_new_obra);
+        button_newvisit = findViewById(R.id.button_new_visit);
+        fromBottom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.from_button_anim);
+        rotateOpen = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_open_anim);
+        toBottom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.to_button_anim);
+        rotateClose = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_close_button);
+        tno = findViewById(R.id.tvobra);
+        tnv = findViewById(R.id.tvvisita);
 
 
         mProgressBar = findViewById(R.id.mProgressBarLoad);
@@ -65,6 +87,10 @@ public class HomeActivity extends AppCompatActivity implements SearchView.OnQuer
         rv.setAdapter(adapter);
 
         bindDades();
+
+        disableFrameLayer();
+
+
 /*
         Visita v1 = new Visita("12-12-12","13:20","Ramon","Restauracio de pis","La infrastructura bla bla", "-MLhWLAAAAArlOKFjNXj");
         mDatabaseRef.child("Visita").child("-MLhwvv4r32r313D8gsh").push().setValue(v1).
@@ -82,7 +108,6 @@ public class HomeActivity extends AppCompatActivity implements SearchView.OnQuer
 
 */
     }
-
 
 
     private void bindDades(){
@@ -138,5 +163,69 @@ public class HomeActivity extends AppCompatActivity implements SearchView.OnQuer
         rv.setLayoutManager(new LinearLayoutManager(this));
         rv.setAdapter(adapter);
         return false;
+    }
+
+    public void newobra_click(View view) {
+        Toast.makeText(this, "New Obra Click", Toast.LENGTH_SHORT).show();
+    }
+    public void newvisit_click(View view) {
+        Toast.makeText(this, "New Visit Click", Toast.LENGTH_SHORT).show();
+    }
+    public void addnew_click(View view) {
+
+        if(!clicked){
+            //SET VISIBILITY
+            button_newobra.setVisibility(View.VISIBLE);
+            button_newvisit.setVisibility(View.VISIBLE);
+            layer_transparent.setVisibility(View.VISIBLE);
+            tno.setVisibility(View.VISIBLE);
+            tnv.setVisibility(View.VISIBLE);
+
+            //SET ANIMATION
+            button_new.startAnimation(rotateOpen);
+            button_newobra.startAnimation(fromBottom);
+            button_newvisit.startAnimation(fromBottom);
+            tno.startAnimation(fromBottom);
+            tnv.startAnimation(fromBottom);
+
+            //CLICKABLE
+            button_newobra.setClickable(true);
+            button_newvisit.setClickable(true);
+
+            clicked = true;
+
+        }else{
+            //SET VISIBILITY
+            button_newobra.setVisibility(View.INVISIBLE);
+            button_newvisit.setVisibility(View.INVISIBLE);
+            layer_transparent.setVisibility(View.INVISIBLE);
+            tno.setVisibility(View.INVISIBLE);
+            tnv.setVisibility(View.INVISIBLE);
+
+            //SET ANIMATION
+            button_new.startAnimation(rotateClose);
+            button_newobra.startAnimation(toBottom);
+            button_newvisit.startAnimation(toBottom);
+            tno.startAnimation(toBottom);
+            tnv.startAnimation(toBottom);
+
+            //CLICKABLE
+            button_newobra.setClickable(false);
+            button_newvisit.setClickable(false);
+
+            clicked = false;
+        }
+    }
+
+    private void disableFrameLayer() {
+        layer_transparent.setOnTouchListener(new FrameLayout.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                addnew_click(v);
+                return false;
+            }
+
+        });
     }
 }
